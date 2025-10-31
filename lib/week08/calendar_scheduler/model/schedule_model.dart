e817986a0822956d0b3b7d56bb5bde2e08adebbb
AlphaXen <kimminjae771@naver.com>
2025-10-31 16:10:01 +0900

    Updated Changes

---
+++ lib/week08/calendar_scheduler/model/schedule_model.dart
@@ -0,0 +1,16 @@
+class ScheduleModel {
+  final String id;
+  final Strring content;
+  final DateTime date;
+  final int startTime;
+  final int endTime;
+}
+
+ScheduleModel({
+  required this.id,
+  required this.content,
+  required this.date,
+  required this.date,
+  required this.startTime,
+  required this.endTime,
+});
