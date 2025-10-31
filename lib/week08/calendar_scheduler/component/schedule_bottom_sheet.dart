a77cb494a52f448350bc82f65319e657fcd3233e
AlphaXen <kimminjae771@naver.com>
2025-10-31 15:55:29 +0900

    Updated Changes

---
+++ lib/week08/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -0,0 +1,156 @@
+import 'package:myapp/week07/calendar_scheduler/component/custom_text_field.dart';
+import 'package:myapp/week07/calendar_scheduler/const/color.dart';
+import 'package:flutter/material.dart';
+import 'package:drift/drift.dart' hide Column;
+import 'package:get_it/get_it.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+
+class ScheduleBottomSheet extends StatefulWidget {
+  final DateTime selectedDate; // 선택된 날짜 상위 위젯에서 입력받기
+
+  const ScheduleBottomSheet({required this.selectedDate, super.key});
+
+  @override
+  State<ScheduleBottomSheet> createState() => _ScheduleBottomState();
+}
+
+class _ScheduleBottomState extends State<ScheduleBottomSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey(); // 폼 키 생성
+
+  int? startTime; //  시작 시간 저장 변수
+  int? endTime; // 종료 시간 저장 변수
+  String? content; // 일정 내용 저장 변수
+
+  @override
+  Widget build(BuildContext context) {
+    // 키보드높이 가져오기
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
+    return Form(
+      // 텍스트 필드를 한 번에 관리할 수 있는 폼
+      key: formKey, // From을 조작할 키값
+      child: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 8,
+              right: 8,
+              top: 8,
+              bottom: bottomInset,
+            ), // 패딩에 키보드 높이 추가해서 위젯 전반적으로 위로 올려주기
+            child: Column(
+              // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
+              children: [
+                Row(
+                  // 시작 시간, 종료 시간 가로로 배치
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        // 시작 시간 입력 필드
+                        label: '시작 시간',
+                        isTime: true,
+                        onSaved: (String? val) {
+                          // 저장이 실행되면 startTime 변수에 텍스트 필드값 저장
+                          startTime = int.parse(val!);
+                        },
+                        validator: timeValidator,
+                      ),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: CustomTextField(
+                        // 종료 시간 입력 필드
+                        label: '종료 시간',
+                        isTime: true,
+                        onSaved: (String? val) {
+                          // 저장이 실행되면 endTime 변수에 텍스트 필드값 저장
+                          endTime = int.parse(val!);
+                        },
+                        validator: timeValidator,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Expanded(
+                  child: CustomTextField(
+                    // 내용 입력 필드
+                    label: '내용',
+                    isTime: false,
+                    onSaved: (String? val) {
+                      // 저장이 실행되면 content 변수에 텍스트 필드값 저장
+                      content = val;
+                    },
+                    validator: contentValidator,
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    // [저장] 버튼
+                    onPressed: onSavePressed,
+                    // [저장] 버튼
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: PRIMARY_COLOR,
+                    ),
+                    child: Text('저장'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onSavePressed() async {
+    if (formKey.currentState!.validate()) {
+      // 폼 검증하기
+      formKey.currentState!.save(); // 폼 저장하기
+
+      await GetIt.I<LocalDatabase>().createSchedule(
+        SchedulesCompanion(
+          startTime: Value(startTime!),
+          endTime: Value(endTime!),
+          date: Value(widget.selectedDate),
+        ),
+      );
+
+      Navigator.of(context).pop(); // 일정 생성 후 화면 뒤로 가기
+    }
+  }
+
+  String? timeValidator(String? val) {
+    // 시간 검증 함수
+    if (val == null) {
+      return '값을 입력해주세요';
+    }
+
+    int? number;
+
+    try {
+      number = int.parse(val);
+    } catch (e) {
+      return '숫자를 입력하여 주세요';
+    }
+
+    if (number < 0 || number > 24) {
+      return '0시부터 24시 사이를 입력해주세요';
+    }
+
+    return null;
+  } // 시간값 검증
+
+  String? contentValidator(String? val) {
+    // 내용 검증 함수
+    if (val == null || val.isEmpty) {
+      return '값을 입력하여 주세요';
+    }
+
+    return null;
+  } // 내용값 검증
+}
---
+++ lib/week08/calendar_scheduler/screen/home_screen.dart
@@ -0,0 +1,138 @@
+import 'package:myapp/week07/calendar_scheduler/component/main_calendar.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/week07/calendar_scheduler/component/schedule_card.dart';
+import 'package:myapp/week07/calendar_scheduler/component/today_banner.dart';
+import 'package:myapp/week07/calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:myapp/week07/calendar_scheduler/const/color.dart';
+import 'package:get_it/get_it.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    // 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        // 새 일정 버튼
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            // BottomSheet 열기
+            context: context,
+            isDismissible: true, // 배경 탭했을 때 BottomSheet 닫기
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate, // 선택된 날짜 (selectedDate) 넘겨주기
+            ),
+            // BottomSheet의 높이를 화면의 최대 높이로
+            // 정의하고 스크롤 가능하게 변경
+            isScrollControlled: true,
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      body: SafeArea(
+        // 시스템 UI 피해서 UI 구현하기
+        child: Column(
+          // 달력과 리스트를 세로로 배치
+          children: [
+            // 미리 작업해둔 달력 위젯 보여주기
+            MainCalendar(
+              selectedDate: selectedDate, // 선택된 날짜 전달하기
+              // 날짜가 선택되었을 때 실행할 함수
+              onDaySelected: onDaySelected, // 선택된 날짜
+            ),
+            SizedBox(height: 8.0),
+            StreamBuilder<List<Schedule>>(
+              // 일정 Stream으로 받아오기
+              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+              builder: (context, snapshot) {
+                return TodayBanner(
+                  selectedDate: selectedDate,
+                  count: snapshot.data?.length ?? 0, // 일정 개수 입력해주기
+                );
+              },
+            ),
+            SizedBox(height: 8.0),
+            Expanded(
+              // 남는 공간을 모두 차지하기
+              // 일정 정보가 Stream으로 제공되기 때문에 StreamBuilder 사용
+              child: StreamBuilder<List<Schedule>>(
+                stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    // 데이터가 없을 때
+                    return Container();
+                  }
+                  // 화면에 보이는 값들만 렌더링하는 리스트
+                  return ListView.builder(
+                    // 리스트에 입력할 값들의 총 개수
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      // 현재 index에 해당되는 일정
+                      final schedule = snapshot.data![index];
+
+                      return Dismissible(
+                        key: ObjectKey(schedule.id), // 유니크한 키값
+                        direction: DismissDirection.startToEnd,
+                        // 밀기 방향(왼쪽에서 오른쪽으로)
+                        onDismissed: (DismissDirection direction) {
+                          // 밀기 했을 때 실행할 함수
+                          GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
+                        },
+                        child: Padding(
+                          // 좌우로 패딩을 추가해서 UI 개선
+                          padding: const EdgeInsets.only(
+                            bottom: 8.0,
+                            left: 8.0,
+                            right: 8.0,
+                          ),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            TodayBanner(
+              // 배너 추가하기
+              selectedDate: selectedDate,
+              count: 0,
+            ),
+            SizedBox(height: 8.0),
+            ScheduleCard(
+              // 구현해둔 일정 카드
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    // 날짜가 선택될 때마다 실행할 함수
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+}
