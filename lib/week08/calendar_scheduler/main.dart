88784cfd42c18f0b841c279e1b64b368fa262c19
AlphaXen <kimminjae771@naver.com>
2025-10-31 17:14:47 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/main.dart
+++ lib/week08/calendar_scheduler/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:myapp/week08/calendar_scheduler/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
