5277ed11a535a7f817ceee4ab5aee5e5cd5bf4de
AlphaXen <kimminjae771@naver.com>
2025-10-31 16:53:17 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/provider/schedule_provider.dart
+++ lib/week08/calendar_scheduler/provider/schedule_provider.dart
@@ -1,2 +1,14 @@
 import 'package:myapp/week08/calendar_scheduler/model/schedule_model.dart';
-import 'package:myapp/week08/calendar_scheduler/model/';
+import 'package:myapp/week08/calendar_scheduler/repository/schedule_repository.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ScheduleProvider extends ChangeNotifier {
+  final ScheduleRepository repository; // API 요청 로직을 담은 클래스
+
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    
+  )
+}
