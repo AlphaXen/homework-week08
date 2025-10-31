c2e843a2f796a77b5545f673e5bee4b7b1192537
AlphaXen <kimminjae771@naver.com>
2025-10-31 17:57:48 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/provider/schedule_provider.dart
+++ lib/week08/calendar_scheduler/provider/schedule_provider.dart
@@ -88,7 +88,9 @@ void deleteSchedule({
   required DateTime date,
   required String id,
 }) async {
-  final resp = await repository.deleteSchedule(id: id);
+  final targetSchedule = cache[date]!.firstWhere(
+    (e) => e.id == id,
+  ); // 삭제할 일정 기억
 
   cache.update( // 캐시에서 데이터 삭제
     date,
