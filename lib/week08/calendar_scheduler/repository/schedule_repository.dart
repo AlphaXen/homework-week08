c210d2e9cab8ee808d68bd783df877579bcd7779
AlphaXen <kimminjae771@naver.com>
2025-10-31 17:34:47 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/repository/schedule_repository.dart
+++ lib/week08/calendar_scheduler/repository/schedule_repository.dart
@@ -5,7 +5,7 @@ import 'package:myapp/week08/calendar_scheduler/model/schedule_model.dart';
 import 'package:dio/dio.dart';
 
 class ScheduleRepository {
-  final _dio = Dio;
+  final _dio = Dio();
   final _targetUrl = 'http//${Platform.isAndroid ? '10.0.2.2': 'localhost'}:3000/schedule'; // 안드로이드에서는 10.0.2.2가 localhost에 해당함
 
   Future<List<ScheduleModel>> getSchedules({
