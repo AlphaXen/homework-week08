04af7775a1c55d6fa8974dd0d0b35ee5596a3ca2
AlphaXen <kimminjae771@naver.com>
2025-10-31 16:50:47 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/provider/schedule_provider.dart
+++ lib/week08/calendar_scheduler/provider/schedule_provider.dart
@@ -1,1 +1,2 @@
-import 'package:myapp/week08/calendar_scheduler/'
+import 'package:myapp/week08/calendar_scheduler/model/schedule_model.dart';
+import 'package:myapp/week08/calendar_scheduler/model/';
