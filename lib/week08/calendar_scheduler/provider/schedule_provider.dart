929b3b6a5769108350feab411d0c2451d8b45c59
AlphaXen <kimminjae771@naver.com>
2025-10-31 17:56:47 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/provider/schedule_provider.dart
+++ lib/week08/calendar_scheduler/provider/schedule_provider.dart
@@ -81,24 +81,6 @@ void createSchedule({
     );
   }
 
-  final savedSchedule = await repository.createSchedule(schedule: schedule);
-
-  cache.update(
-    targetDate,
-    (value) => [ // 현존하는 캐시 리스트 끝에 새로운 일정 추가
-      ...value,
-      schedule.copyWith(
-        id: savedSchedule,
-      ),
-    ].. sort(
-      (a, b) => a.startTime.compareTo(
-        b.startTime,
-      ),
-    ),
-    // 날짜에 해당되는값이 없다면 새로운 리스트에 새로운 일정 하나만 추가
-    ifAbsent: () => [schedule],
-  );
-
   notifyListeners();
 }
 
