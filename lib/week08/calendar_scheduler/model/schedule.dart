359c09cdb920c7ae2aa7d32333511e6a75359ef8
AlphaXen <kimminjae771@naver.com>
2025-10-31 16:11:15 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/model/schedule.dart
+++ lib/week08/calendar_scheduler/model/schedule.dart
@@ -1,16 +1,9 @@
-class ScheduleModel {
-  final String id;
-  final Strring content;
-  final DateTime date;
-  final int startTime;
-  final int endTime;
-}
+import 'package:drift/drift.dart';
 
-ScheduleModel({
-  required this.id,
-  required this.content,
-  required this.date,
-  required this.date,
-  required this.startTime,
-  required this.endTime,
-});
+class Schedules extends Table {
+  IntColumn get id => integer().autoIncrement()(); // PRIMARY KEY, 정수 열
+  TextColumn get content => text()(); // 내용, 글자 열
+  DateTimeColumn get date => dateTime()(); // 일정 날짜, 날짜 열
+  IntColumn get startTime => integer()(); // 시작 시간
+  IntColumn get endTime => integer()(); // 종료 시간
+}
